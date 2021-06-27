f65bdf15650daff461c2326b42a79fcb9a1830e8
matthewes1 <43347136+matthewes1@users.noreply.github.com>
2021-06-27 18:05:31 -0400

    Changed Adulthood and Childhood code to use the identifier instead of name. Also added some code that is commented out that allows for creating a text file with all identifiers since they are locked away in unity assets

--- SpeakUp/ModBaseSpeakUp.cs
+++ SpeakUp/ModBaseSpeakUp.cs
@@ -1,5 +1,9 @@
 ﻿using HugsLib;
 using HugsLib.Settings;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace SpeakUp
 {
@@ -28,6 +32,30 @@ namespace SpeakUp
         public override void DefsLoaded()
         {
             UpdateSettings();
+
+            /*
+            //The following is for childhoods and adulthoods. They are story in Unity asset files so we cannot view the XML. Some childhoods/adulthoods have the same generic name but a unique description and identifier. This portion of code creates a text file with ALL childhood and adulthood descriptions, names, and unique identifiers
+            string export = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(export, "backstories.txt"), true))
+            {
+                foreach (KeyValuePair<string, Backstory> story in BackstoryDatabase.allBackstories)
+                {
+                    String backstory;
+                    if (story.Value.slot == BackstorySlot.Childhood)
+                    {
+                        backstory = "Childhood: ";
+                    }
+                    else
+                    {
+                        backstory = "Adulthood: ";
+                    }
+
+                    backstory += story.Value.identifier + " = ";
+                    backstory += story.Value.baseDesc;
+                    outputFile.WriteLine(backstory);
+                    outputFile.WriteLine();
+                }
+            }*/
         }
 
         public void UpdateSettings()
