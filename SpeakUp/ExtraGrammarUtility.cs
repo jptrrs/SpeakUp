bb8d7ed3ce960ee2548f1270331481b2d6489e8c
João Pedro Torres <jaopedro@gmail.com>
2021-07-02 13:13:59 -0300

    Simplified backstory nullcheck and preparing for launch.

--- SpeakUp/ExtraGrammarUtility.cs
+++ SpeakUp/ExtraGrammarUtility.cs
@@ -116,12 +116,14 @@ namespace SpeakUp
             }
 
             //childhood
-            if (pawn.story.childhood != null) MakeRule(symbol + "childhood", pawn.story.childhood.identifier);
+            MakeRule(symbol + "childhood", pawn.story.childhood?.identifier);
 
             //adulthood
-            if (pawn.story.adulthood != null) MakeRule(symbol + "adulthood", pawn.story.adulthood.identifier);
+            MakeRule(symbol + "adulthood", pawn.story.adulthood?.identifier);
 
             //OTHER PAWN SITUATIONS
+
+            //moving?
             MakeRule(symbol + "moving", pawn.pather.Moving.ToStringYesNo());
 
             //current activity
